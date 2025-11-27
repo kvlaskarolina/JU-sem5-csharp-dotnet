60c55261aa7ec6b82c9f605097df4c38d4fef3fe
karo <karolinykulas@gmail.com>
2025-11-27 23:57:24 +0100

    template

--- lab01-131025/obj/Debug/net9.0/lab01-131025.AssemblyInfo.cs
+++ lab01-131025/obj/Debug/net9.0/lab01-131025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab01-131025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbac426248c53c75af19d38813e83b29487e74f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8237be22a082bf00c82c711d1931711fa74edd")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab01-131025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab01-131025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
