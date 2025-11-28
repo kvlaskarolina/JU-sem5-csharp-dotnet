ae6536166ee80f28eb76ee3e63c3f8c90270d2e7
karo <karolinykulas@gmail.com>
2025-11-28 22:25:49 +0100

    z4.1 done:)

--- lab04/z4.1/Program.cs
+++ lab04/z4.1/Program.cs
@@ -1,2 +1,19 @@
 ﻿//  https://github.com/kvlaskarolina/csharp-dotnet
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        IEnumerable<int> nums = Enumerable.Range(1, n);
+        var squares = nums.Where(x => x != 5 && x != 9 && (x % 2 == 1 || x % 7 == 0)).Select(x => x * x);
+        //Console.WriteLine(string.Join(", ", squares)); // to test:)
+        Console.WriteLine(squares.Sum());
+        Console.WriteLine(squares.Count());
+        Console.WriteLine(squares.First());
+        Console.WriteLine(squares.Last());
+        Console.WriteLine(squares.ElementAt(2));
+    }
+}
