8437712dc78494aa41e3359d8a55993cdc12b13c
karo <karolinykulas@gmail.com>
2025-11-29 15:35:51 +0100

    4.2 done:)

--- lab04/z4.2/Program.cs
+++ lab04/z4.2/Program.cs
@@ -1,2 +1,33 @@
 ﻿//  https://github.com/kvlaskarolina/csharp-dotnet
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var m = int.Parse(Console.ReadLine());
+        Random random = new Random();
+
+        var matrixNxM = Enumerable.Range(0, n)
+            .Select(_ =>
+            {
+                return Enumerable.Range(0, m)
+                    .Select(col => random.Next(1, 101))
+                    .ToList();
+            })
+            .ToList();
+
+        var suma = matrixNxM
+            .SelectMany(wiersz => wiersz)
+            .Sum(x => (long)x);
+
+        Console.WriteLine(suma);
+
+        string macierzStr = string.Join("\n",
+            matrixNxM.Select(wiersz => string.Join(" ", wiersz)));
+        Console.WriteLine(macierzStr);
+    }
+}
